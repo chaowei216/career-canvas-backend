6ad4ef7958423ba0ef32b051a78d0ff90ca230a4
Luu Trieu Vi (DX) <luutrieuvi2003@gmail.com>
2025-06-20 14:47:05 +0700

    feat: init project structure

---
+++ src/main/java/com/weiz/careercanvas/CareerCanvasApplication.java
@@ -0,0 +1,13 @@
+package com.weiz.careercanvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerCanvasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerCanvasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weiz/careercanvas/CareerCanvasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weiz.careercanvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerCanvasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
